3b6a932236ec5b2daec8aefd7872d986946c5230
Nietosvuori Kati Maria <kati.m.nietosvuori@student.jyu.fi>
2024-03-05 19:41:55 +0200

    add city noise map, clean code

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MapGenerator : MonoBehaviour
 {
@@ -17,6 +19,7 @@ public class MapGenerator : MonoBehaviour
     public Color groundColor;
     public Color mountainColor;
     public Color snowColor;
+    public Color cityColor;
 
     // Map settings.
     [Header("General Settings")]
@@ -33,6 +36,15 @@ public class MapGenerator : MonoBehaviour
     public float lacunarity;
     public int seed;
 
+    // Same settings for generating the city noise map
+    [Header("City noise Settings")]
+    public float cityNoiseScale;
+    public int cityOctaves;
+    [Range(0, 1)] public float cityPersistence;
+    public float cityLacunarity;
+    public int citySeed;
+
+    [Header("Color limits")]
     // Range sliders for noise map values.
     [Range(0, 1)] public float oceanLimit;
     [Range(0, 1)] public float lowOceanLimit;
@@ -41,6 +53,10 @@ public class MapGenerator : MonoBehaviour
     [Range(0, 1)] public float groundLimit;
     [Range(0, 1)] public float mountainLimit;
     [Range(0, 1)] public float snowLimit;
+    [Range(0, 1)] public float cityLimit;
+
+    // Define limit and color pairs for cleaner logic
+    public List<KeyValuePair<float, Color>> colorRanges = new List<KeyValuePair<float, Color>>();
 
     /// <summary>
     /// Generates the noise map.
@@ -49,9 +65,11 @@ public class MapGenerator : MonoBehaviour
     {
         // If you want to generate additional noisemaps, you can call the function many times with randomized seeds and different options.
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
+        // Another noise map for for city generation
+        float[,] cityNoiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, citySeed, cityNoiseScale, cityOctaves, cityPersistence, cityLacunarity, offset);
 
         // This draws the map (so don't remove it).
-        DrawNoiseMap(noiseMap);
+        DrawNoiseMap(noiseMap, cityNoiseMap);
     }
 
     /// <summary>
@@ -69,7 +87,7 @@ public class MapGenerator : MonoBehaviour
     /// Draws the noise map from a two dimensional array.
     /// </summary>
     /// <param name="noiseMap">Generated noise map</param>
-    public void DrawNoiseMap(float[,] noiseMap)
+    public void DrawNoiseMap(float[,] noiseMap, float[,] cityNoiseMap)
     {
         int width = noiseMap.GetLength(0);
         int height = noiseMap.GetLength(1);
@@ -77,36 +95,35 @@ public class MapGenerator : MonoBehaviour
         Texture2D texture = new Texture2D(width, height);
 
         Color[] colourMap = new Color[width * height];
+
+        // Add all the limit color pairs to the List
+        colorRanges.Add(new KeyValuePair<float, Color>(snowLimit, snowColor));
+        colorRanges.Add(new KeyValuePair<float, Color>(mountainLimit, mountainColor));
+        colorRanges.Add(new KeyValuePair<float, Color>(groundLimit, groundColor));
+        colorRanges.Add(new KeyValuePair<float, Color>(lightGroundLimit, lightGroundColor));
+        colorRanges.Add(new KeyValuePair<float, Color>(beachLimit, beachColor));
+        colorRanges.Add(new KeyValuePair<float, Color>(lowOceanLimit, lowOceanColor));
+        colorRanges.Add(new KeyValuePair<float, Color>(oceanLimit, oceanColor));
+        colorRanges.Add(new KeyValuePair<float, Color>(0, deepOceanColor));
+
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
                 // This is how you get the value at a location in the noise map.
                 float noise = noiseMap[x, y];
+                float cityNoise = cityNoiseMap[x, y];
 
-                // Here we set the colours of each pixel based on their noise map's value.
-                if (noise > snowLimit) {
-                    colourMap[y * width + x] = snowColor;
-                } else if (noise > mountainLimit) {
-                    colourMap[y * width + x] = mountainColor;
-                } else if (noise > groundLimit) {
-                    colourMap[y * width + x] = groundColor;
-                } else if (noise > lightGroundLimit) {
-                    colourMap[y * width + x] = lightGroundColor;
-                } else if (noise > beachLimit) {
-                    colourMap[y * width + x] = beachColor;
-                } else if (noise > lowOceanLimit) {
-                    colourMap[y * width + x] = lowOceanColor;
-                } else if (noise > oceanLimit) {
-                    colourMap[y * width + x] = oceanColor;
-                } else {
-                    colourMap[y * width + x] = deepOceanColor;
+                // Here we set the colours of each pixel based on their noise map's value:
+                // Find the first color range where noise falls within the limit and use that color
+                Color chosenColor = colorRanges.Find(range => noise > range.Key).Value;
+                colourMap[y * width + x] = chosenColor;
+
+                // Cities appear between ocean and ground limits
+                // Use the separate cityNoiseMap to generate the locations in these limits
+                if (noise > beachLimit && noise < groundLimit && cityNoise > cityLimit) {
+                    colourMap[y * width + x] = cityColor;
                 }
-                
-                // You can add if-else clauses or other bits of logic to add
-                // different colors based on the noise map's value.
-
-
             }
         }
 
